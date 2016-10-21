9593541b89ef35c9ab6da070da8e58927a752724
Jake Barnwell <jbarnwell@mit.edu>
2016-10-20 23:45:56 -0400

    Add tests for gradient descent

--- lab6.py
+++ lab6.py
@@ -18,11 +18,9 @@ nn_stripe = []
 
 nn_hexagon = []
 
-# Optional problem; change TEST_NN_GRID to True to test locally
-TEST_NN_GRID = False
 nn_grid = []
 
-# Helper functions
+# Thresholding functions
 def stairstep(x, threshold=0):
     "Computes stairstep(x) using the given threshold (T)"
     raise NotImplementedError
@@ -31,8 +29,20 @@ def sigmoid(x, steepness=1, midpoint=0):
     "Computes sigmoid(x) using the given steepness (S) and midpoint (M)"
     raise NotImplementedError
 
+def ReLU(x):
+    "Computes the threshold of an input using a rectified linear unit."
+    raise NotImplementedError
+
+# Helper functions
 def accuracy(desired_output, actual_output):
     "Computes accuracy. If output is binary, accuracy ranges from -0.5 to 0."
+    raise NotImplementedError
+
+def node_value(node, input_values, neuron_outputs):
+    """Given a node in the neural net, as well as a dictionary 
+    of neural net input values and a dictionary mapping neuron 
+    names to their outputs, computes the effective value of this 
+    node."""
     raise NotImplementedError
 
 # Forward propagation
@@ -44,23 +54,45 @@ def forward_prop(net, input_values, threshold_fn=stairstep):
     (2) a dictionary mapping neurons to their immediate outputs"""
     raise NotImplementedError
 
+# Backward propagation warmup
+def gradient_step(func, values, delta):
+    """Given some unknown function of three variables and a list of three 
+    values representing the current inputs into the function,
+    finds the amount that the function changes
+    by varying (or not) each of the input variables by +/- delta (a total 
+    3^3 = 27 possible assignments for the three variables). Picks 
+    the assignments of variables that yields the smallest result when 
+    input into the function, and returns a tuple containing
+    (1) the function value at the lowest point found, and 
+    (2) the list of variable assignments that yielded the lowest
+    function value."""
+    raise NotImplementedError
+
+def calculate_back_prop_dependencies(net, wire):
+    """Given a wire in a neural network, returns a set of inputs, neurons,
+    and Wires whose outputs/values are required to compute the 
+    delta_B coefficient required to update this wire's weight."""
+    raise NotImplementedError 
+
 # Backward propagation
-def calculate_deltas(net, input_values, desired_output):
-    """Computes the update coefficient (delta_B) for each neuron in the
-    neural net.  Uses sigmoid function to compute output.  Returns a dictionary
-    mapping neuron names to update coefficient (delta_B values)."""
+def calculate_deltas(net, desired_output, neuron_outputs):
+    """Given a neural net and a dictionary of neuron outputs from forward-
+    propagation, computes the update coefficient (delta_B) for each
+    neuron in the net. Uses the sigmoid function to compute neuron output.
+    Returns a dictionary mapping neuron names to update coefficient (the 
+    delta_B values). """
     raise NotImplementedError
 
-def update_weights(net, input_values, desired_output, r=1):
+def update_weights(net, input_values, desired_output, neuron_outputs, r=1):
     """Performs a single step of back-propagation.  Computes delta_B values and
-    weight updates for entire neural net, then updates all weights.  Uses
-    sigmoid function to compute output.  Returns the modified neural net, with
-    updated weights."""
+    weight updates for entire neural net, then updates all weights.  Uses the
+    sigmoid function to compute neuron output.  Returns the modified neural net,
+    with the updated weights."""
     raise NotImplementedError
 
-def back_prop(net, input_values, desired_output, r=1, accuracy_threshold=-.001):
-    """Updates weights until accuracy surpasses minimum_accuracy.  Uses sigmoid
-    function to compute output.  Returns a tuple containing:
+def back_prop(net, input_values, desired_output, r=1, minimum_accuracy=-0.001):
+    """Updates weights until accuracy surpasses minimum_accuracy.  Uses the sigmoid
+    function to compute neuron output.  Returns a tuple containing:
     (1) the modified neural net, with trained weights
     (2) the number of iterations (that is, the number of weight updates)"""
     raise NotImplementedError
--- nn_problems.py
+++ nn_problems.py
@@ -14,6 +14,8 @@ nn_AND = NeuralNet(['x', 'y', -1], ['N1']) \
     .join('x', 'N1').join('y', 'N1').join(-1, 'N1', 1.5) \
     .join('N1', NeuralNet.OUT)
 
+nn_AND_input = {'x':3.5, 'y':-2}
+
 # Results of taining nn_AND with inputs x=3.5, y=-2 and desired_output=1
 nn_AND_update_iter1 = NeuralNet(['x', 'y', -1], ['N1']) \
     .join('x', 'N1', 1.4375).join('y', 'N1', 0.75).join(-1, 'N1', 1.375) \
@@ -53,6 +55,7 @@ def get_nn_River(w1, w2, w3, w4, w5, w6):
         .join('C', NeuralNet.OUT)
     return nn_River
 nn_River_inputs = {'M':1, 'S':0, 'G':1}  # River's features
+nn_River_fwd_prop1 = {'A': 0.7310585786300049, 'C': 0.839846413654423, 'B': 0.11920292202211757}
 nn_River_desired = 0  # River is a human
 
 nn_branching = NeuralNet(['in'], ['Nin', 'N1', 'N2', 'N3', 'Nout']) \
@@ -60,7 +63,9 @@ nn_branching = NeuralNet(['in'], ['Nin', 'N1', 'N2', 'N3', 'Nout']) \
     .join('Nin', 'N1', 1).join('Nin', 'N2', 2).join('Nin', 'N3', 3) \
     .join('N1', 'Nout', 1).join('N2', 'Nout', 1).join('N3', 'Nout', -3) \
     .join('Nout', NeuralNet.OUT)
-
+nn_branching_input = {'in': 17}
+# Results from starting with 'in':17 and with desired output = 1
+nn_branching_fwd_prop1 = {'N1': 0.6224593312018546, 'N2': 0.7310585786300049, 'N3': 0.8175744761936437, 'Nin': 0.5, 'Nout': 0.24988878585697502}
 nn_branching_update_iter1 = NeuralNet(['in'], ['Nin','N1','N2','N3','Nout']) \
     .join('in', 'Nin', -0.426717312066) \
     .join('Nin', 'N1', 1.01652124647) \
